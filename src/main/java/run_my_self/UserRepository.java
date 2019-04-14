aba84c43b4a56cec268af717cd43700369425485
shaoyue.chen <shaoyue.chen@ele.me>
2019-04-14 23:49:07 +0800

    fix dir

--- src/main/java/run_my_self/UserRepository.java
+++ src/main/java/run_my_self/UserRepository.java
@@ -2,6 +2,8 @@ package run_my_self;
 
 import org.springframework.data.repository.CrudRepository;
 
+import run_my_self.User;
+
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
